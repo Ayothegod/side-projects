d3b1fee81848addc4c669bf19522e58f9066d601
Ayo <heyayomideadebisi@gmail.com>
2024-03-11 11:06:04 +0100

    feat: start dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import { Button } from "@/components/ui/button";
 
 function App() {
-
   return (
-    <div>
-     <p className="text-4xl text-red-600">Hello react app</p>
+    <div className="m-10">
+      <p className="text-4xl text-red-600">Hello react app</p>
+      <p className="text-xl text-violet-600">Reactjs side projects</p>
+      <Button variant="default">Click me</Button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
